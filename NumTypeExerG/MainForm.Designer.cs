7b6c17645dbc16fc62de5bdac84a1643346e79a8
ricky50575 <molesg@yeah.net>
2021-07-29 23:13:12 +0800

    新增图形界面

---
+++ NumTypeExerG/MainForm.Designer.cs
@@ -0,0 +1,176 @@
+﻿
+namespace NumTypeExerG
+{
+    partial class MainForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
+            this.numericUpDown1.Location = new System.Drawing.Point(122, 7);
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(42, 23);
+            this.numericUpDown1.TabIndex = 0;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            75,
+            0,
+            0,
+            0});
+            this.numericUpDown1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.numericUpDown1_KeyDown);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(104, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "生成的数字位数：";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(179, 7);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(93, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "开始练习(F1)";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Location = new System.Drawing.Point(277, 7);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(93, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "停止练习(F2)";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(377, 10);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(300, 17);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "已用时长：0秒  已经完成：0组  平均速度：0.0组/分钟";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 40);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(776, 12);
+            this.progressBar1.Step = 1;
+            this.progressBar1.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
+            this.progressBar1.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Consolas", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(8, 55);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(734, 32);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "按“开始练习”按钮或快捷键“F1”开始练习，“F2”停止。";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Enabled = false;
+            this.textBox1.Font = new System.Drawing.Font("Consolas", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBox1.Location = new System.Drawing.Point(12, 90);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(776, 39);
+            this.textBox1.TabIndex = 7;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.textBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyDown);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(799, 140);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numericUpDown1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.Text = "贴贴我的百花酱！数字打字练习";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.MainForm_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ NumTypeExerG/MainForm.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NumTypeExerG
+{
+    public partial class MainForm : Form
+    {
+        public static long used_time;
+        public static int sum;
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void newNum()
+        {
+            Random ran = new Random();
+            label3.Text = "";
+            textBox1.Text = "";
+            for (int i = 1; i <= Convert.ToInt32(numericUpDown1.Value); i++)
+            {
+                label3.Text = label3.Text + Convert.ToString(ran.Next(0, 9));
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            progressBar1.Maximum = Convert.ToInt32(numericUpDown1.Value);
+            button1.Enabled = false;
+            button2.Enabled = true;
+            numericUpDown1.Enabled = false;
+            used_time = 0;
+            sum = 0;
+            label3.Width = Convert.ToInt32(Convert.ToDouble(numericUpDown1.Value) * 1.514);
+            newNum();
+            progressBar1.Width = label3.Width - 10;
+            textBox1.Width = label3.Width - 10;
+            int windowWidth = label3.Location.X + label3.Width + 20;
+            if(windowWidth <= 815)
+            {
+                this.Width = 815;
+            }
+            else
+            {
+                this.Width = windowWidth;
+            }
+            label2.Text = "已用时长：0秒  已经完成：0组  平均速度：0.0组/分钟";
+            timer1.Enabled = true;
+            textBox1.Enabled = true;
+            textBox1.Focus();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            button1.Enabled = true;
+            button2.Enabled = false;
+            numericUpDown1.Enabled = true;
+            timer1.Enabled = false;
+            textBox1.Enabled = false;
+            numericUpDown1.Focus();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            used_time++;
+            label2.Text = "已用时长：" + used_time.ToString() + "秒  已经完成：" + sum.ToString() + "组  平均速度：" + (Convert.ToDouble(sum) / (Convert.ToDouble(used_time)/60)).ToString("0.0") + "组/分钟";
+        }
+
+        private void MainForm_KeyDown(object sender, KeyEventArgs e)
+        {
+            
+        }
+
+        private void numericUpDown1_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.F1:
+                    button1.PerformClick();
+                    break;
+                case Keys.F2:
+                    button2.PerformClick();
+                    break;
+            }
+        }
+
+        private void textBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.F1:
+                    button1.PerformClick();
+                    break;
+                case Keys.F2:
+                    button2.PerformClick();
+                    break;
+            }
+        }
+
+        public int getSameLen(string s1, string s2)
+        {
+            int min_len = s1.Length > s2.Length ? s2.Length : s1.Length;
+            int same_len = 0;
+            for(int i = 0; i < min_len; i++)
+            {
+                if(s1[i] == s2[i])
+                {
+                    same_len++;
+                }
+                else
+                {
+                    break;
+                }
+            }
+            return same_len;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            progressBar1.Value = getSameLen(label3.Text, textBox1.Text);
+            if(progressBar1.Maximum == progressBar1.Value)
+            {
+                newNum();
+                sum++;
+            }
+        }
+    }
+}
